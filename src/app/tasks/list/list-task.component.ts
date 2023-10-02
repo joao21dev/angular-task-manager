75b36ce2d7489d400ac1bd8f2e8cd7f2db5cb7ee
joao21dev <joao21dev@gmail.com>
2023-10-02 01:46:53 -0300

    feat: deleting task

--- src/app/tasks/list/list-task.component.ts
+++ src/app/tasks/list/list-task.component.ts
@@ -14,4 +14,19 @@ export class ListTaskComponent implements OnInit {
   ngOnInit(): void {
     this.tasks = this.taskService.listAll();
   }
+
+  delete($event: any, task: Task): void {
+    $event.preventDefault();
+    if (confirm('Deseja remover a tarefa "' + task.name + '"?')) {
+      this.taskService.deleteTask(task.id);
+      this.tasks = this.taskService.listAll();
+    }
+  }
+
+  changeStatus(task: Task): void {
+    if (confirm('Deseja alterar o status da tarefa "' + task.name + '"?')) {
+      this.taskService.editIsFinished(task.id);
+      this.tasks = this.taskService.listAll();
+    }
+  }
 }
